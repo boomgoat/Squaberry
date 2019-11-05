463734e75046a63424446e471fc1731e42ce4831
boomgoat <talhaiqbal11@gmail.com>
2019-11-04 22:13:24 -0800

    Contact Form added

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,1 +1,23 @@
 
+$(document).ready(function(){
+    $('.customer-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 800,
+            settings: {
+                slidesToShow: 3
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 2
+            }
+        }]
+    });
+});
